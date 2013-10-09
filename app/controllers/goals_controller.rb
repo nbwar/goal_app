5bb38de5f883ac3bc070555999e4799547a99d1c
Nicholas Wargnier <nwargnier@gmail.com>
2013-10-08 22:41:47 -0700

    unused controller methods cleanup

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -17,10 +17,6 @@ class GoalsController < ApplicationController
     end
   end
 
-  def show
-    @goal = Goal.find(params[:id])
-  end
-
   def show_goal
     goal = Goal.find(params["goal"])
     render :json => render_to_string(:partial => "large_goal", :locals => {goal: goal} ).to_json
@@ -30,10 +26,6 @@ class GoalsController < ApplicationController
     goal = Goal.find(params["goal"])
     object = goal.send(params["klass"].pluralize).create(title: params["title"], description: params["description"])
     render :json => render_to_string(:partial => "goal_attribute", :locals => { object: object }).to_json
-  end
-
-  def edit
-    @goal = Goal.find(params[:id])
   end
 
   def update
